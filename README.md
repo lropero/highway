aefc4fe7e044afc8dfeac3ecaf9eddf2656f5ad9
Luciano Ropero <lropero@gmail.com>
2021-04-27 13:55:46 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# TS 🔍 &middot; [![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active) ![GitHub package.json version](https://img.shields.io/github/package-json/v/lropero/ts)
+
+Times and sales.
+
+### Requires
+
+- [Node v14.16.1](https://nodejs.org/)
+- npm v7.11.1
+
+### Download
+
+- [ts-main.zip](https://github.com/lropero/ts/archive/main.zip) or `git clone https://github.com/lropero/ts.git`
+
+### Installation
+
+```sh
+$ npm ci
+```
+
+### Configuration
+
+- Create `.env` file with your Binance reading-only keys (_required for data retrieval_):
+
+```sh
+APIKEY=<YOUR_API_KEY>
+APISECRET=<YOUR_API_SECRET>
+```
+
+### Usage
+
+```sh
+$ npm run start # will run 'node ts.js -p BTCUSDT'
+```
+
+### Options
+
+##### `-h` / `--help`
+
+Display help
+
+```sh
+node ts.js -h
+```
+
+##### `-b` / `--block <size>`
+
+Block alert size (default 0)
+
+```sh
+node ts.js -b 50
+```
+
+##### `-f` / `--filter <size>`
+
+Filter less than size (default 0)
+
+```sh
+node ts.js -f 10
+```
+
+##### `-p` / `--pair <pair>`
+
+Pair (default BTCUSDT)
+
+```sh
+node ts.js -p ETHUSDT
+```
---
+++ ts.js
@@ -0,0 +1,51 @@
+const Binance = require('node-binance-api')
+const chalk = require('chalk')
+const dotenv = require('dotenv')
+const { cross, line } = require('figures')
+const { format } = require('date-fns')
+const { program } = require('commander')
+
+const { version } = require('./package.json')
+
+dotenv.config()
+
+const binance = new Binance().options({
+  APIKEY: process.env.APIKEY,
+  APISECRET: process.env.APISECRET
+})
+const isWindows = process.platform === 'win32'
+
+const log = message => {
+  console.log(`${chalk[isWindows ? 'white' : 'gray'](format(new Date(), 'HH:mm:ss'))} ${message}`)
+}
+
+const run = async options => {
+  try {
+    console.log(`${chalk.green(`Times & Sales v${version}`)} ${chalk[isWindows ? 'white' : 'gray'](`${line} run with -h to output usage information`)}`)
+    console.log(chalk.yellow(`Like it? Buy me a ${isWindows ? 'beer' : '🍺'} :) 1B7owVfYhLjWLh9NWivQAKJHBcf8Doq54i (BTC)`))
+    binance.websockets.trades(options.pair, trade => {
+      // const { e: eventType, E: eventTime, s: symbol, t: tradeId, p: price, q: quantity, b: buyerOrderId, a: sellerOrderId, T: tradeTime, m: marketMaker } = trade
+      const { p: price, q: quantity, m: marketMaker } = trade
+      if (quantity >= options.filter) {
+        log(chalk[marketMaker ? 'red' : 'green'](`${price} ${options.block > 0 && quantity >= options.block ? chalk.bgWhite(parseFloat(quantity)) : parseFloat(quantity)}`))
+      }
+    })
+  } catch (error) {
+    log(`${chalk.red(cross)} ${error.toString()}`)
+    process.exit(0)
+  }
+}
+
+program
+  .option('-b, --block <size>', 'block alert size (default 0)')
+  .option('-f, --filter <size>', 'filter less than size (default 0)')
+  .option('-p, --pair <pair>', 'pair (default BTCUSDT)')
+  .parse(process.argv)
+
+const options = program.opts()
+
+run({
+  block: options.block ? parseFloat(options.block) : 0,
+  filter: options.filter ? parseFloat(options.filter) : 0,
+  pair: options.pair ?? 'BTCUSDT'
+})
